95bda422765fc782563c6bee0b10a97e193c403b
sorapmas <128443029+sorapmas@users.noreply.github.com>
2023-07-05 20:45:38 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,63 +1,81 @@
 import os
 import csv
 
-budget_data_csv = os.path.join ("Resources", "budget_data.csv")
+election_data_csv = os.path.join ("Resources", "election_data.csv")
+#election_data_csv = '/Users/siykee/Downloads/Starter_Code-3/PyPoll/Resources/election_data.csv'
 
-file_to_save = "file_to_save/analysis"
+#file_to_save = os.path.join ("analysis", "election_results.txt")
+file_to_save = 'file_to_save/analysis'
 
-#initalizing list
-total_months = 0
-month_of_change = []
-net_change_list = []
-greatest_increase = ["", 0]
-greatest_decrease = ["", 9999999999999999999]
-total_net = 0
-# Read the csv and convert it into a list of dictionaries
-with open(budget_data_csv) as budget_data:
-    reader = csv.reader(budget_data)
-    # Read the header row
-    header = next(reader)
-    # Extract first row to avoid appending to net_change_list
-    first_row = next(reader)
-    total_months += 1
-    total_net += int(first_row[1])
-    prev_net = int(first_row[1])
-    for row in reader:
-        # Track the total
-        total_months += 1
-        total_net += int(row[1])
-        # Track the net change
-        net_change = int(row[1]) - prev_net
-        prev_net = int(row[1])
-        net_change_list += [net_change]
-        month_of_change += [row[0]]
-        # Calculate the greatest increase
-        if net_change > greatest_increase[1]:
-            greatest_increase[0] = row[0]
-            greatest_increase[1] = net_change
-        # Calculate the greatest decrease
-        if net_change < greatest_decrease[1]:
-            greatest_decrease[0] = row[0]
-            greatest_decrease[1] = net_change
-# Calculate the Average Net Change
-net_monthly_avg = sum(net_change_list) / len(net_change_list)
-#Output summary
-output = (
-    f"Financial Analysis\n"
-    f"----------------------------\n"
-    f"Total Months: {total_months}\n"
-    f"Total: ${total_net}\n"
-    f"Average Change: ${net_monthly_avg:.2f}\n"
-    f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
-    f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
-# Print output to terminal
-print(output)
+#Initalizing list in dictonary 
+Canidates_list = []
+Canidates_votes = {}
+with open(election_data_csv) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+    #Reading the header
+    header = next(csv_reader)
+    #Initalize total vote counter
+    Total_votes = 0
+    #Read each row of data after header
+    for row in csv_reader:
+        Canidate = row[2]
+        if Canidate not in Canidates_list: 
+            Canidates_list.append(Canidate)
+            #Starting off each canidate at 0
+            Canidates_votes[Canidate]=0
+        #Each time it reads each indiviual canidates name in set, it'll add onto their amount of votes
+        Canidates_votes[Canidate]=Canidates_votes[Canidate] + 1
+        #Asssigning new values onto exisiting variables
+        Total_votes+= 1
+#Printing title and total votes into terminal
+    print(
+       f"\nElection Results\n"
+       f"-----------------------\n"
+       f"Total Votes: {Total_votes:,}\n"
+       f"\n-----------------------\n")
+
+#Printing title and total votes into text file
+    election_results = (
+       f"\nElection Results\n"
+       f"-----------------------\n"
+       f"Total Votes: {Total_votes:,}\n"
+       f"\n-----------------------\n")
+
+#Ruling winning canidate
+winning_count = 0
+winning_percentage = 0
+winning_canidate = ""
+
+#Retrieve each canidates percantages 
+for Canidate_name in Canidates_votes:
+    votes = Canidates_votes.get(Canidate_name)
+    votes_percentage = float(votes) / float(Total_votes) * 100
+    canidate_results = (
+        f"{Canidate_name}: {votes_percentage:.1f}% ({votes:,})\n")
+    print(canidate_results)
+
+#determining winninng canidate
+    if (votes_percentage > winning_percentage ) and (votes > winning_count):
+        winning_percentage = votes_percentage
+        winning_count = votes
+        winning_canidate = Canidate_name
+
+ruling_winning_canidate =(
+    f"-----------------------\n" 
+    f"Winner: {winning_canidate}\n"
+    f"-----------------------\n")
+print(ruling_winning_canidate)
 
 #adding results to text file
 with open(file_to_save, 'w') as txt_file:
-    txt_file.write(output)
+    txt_file.write(election_results)
+    txt_file.write(canidate_results)
+    txt_file.write(ruling_winning_canidate)
 
 
+    
 
-        
+
+    
+
 
